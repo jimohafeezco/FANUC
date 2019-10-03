be5a875f981217a376dcfc1e3d4193a4fb70460a
jimohafeezco <jimohafeezco@gmail.com>
2019-10-03 22:07:04 +0300

    IK for wrist added

--- calcFK_nd_IK_with_tests.m
+++ calcFK_nd_IK_with_tests.m
@@ -1,5 +1,5 @@
 %Declaration of robot parameters
-
+clear all; clc;clf;
 alpha =[-pi/2 0 -pi/2 pi/2 -pi/2 0];
 a1 = 0.312; d1= 0.324;
 a2= 1.075; d2 = 0;
@@ -12,17 +12,20 @@ d=[d1 d2 d3 d4 d5 d6];
 
 % theta can be changed here to see different forward kinematic resukts of
 % the robot
+
+q1 = linspace(-pi,pi,5);
+q2 = linspace(-pi,pi,5);
+
 theta=[q1 q2 0 pi 0 0];
 
 
 % q1 for variable joint 1 and joint 2 angles
-q1 = linspace(-pi,pi,5);
-q2 = linspace(-pi,pi,5);
 
 for i = 1:length(q1)
     for j = 1: length(q2)
     theta(1) = q1(i);
     theta(2) = q2(i);
+
 % Calling of DH function to calculate transformation matrix for each frame.
 DH1 = DH(theta(1), d(1), alpha(1),a(1));
 DH2 = DH(theta(2), d(2), alpha(2),a(2));
@@ -57,6 +60,28 @@ q2_1 = atan2(s2,c2_2)
 q3= q23-q20
 q3_1= q23-q2_1
 
+% computing invere kinematics for the wrist
+
+r60_kine = fkine(1:3, 1:3);
+r30_kine = f_arm(1:3, 1:3);
+r36 = r30_kine' *r60_kine;
+
+nx = r36(1,1);
+ny = r36(2,1);
+nz = r36(3,1);
+sx = r36(1,2);
+sy = r36(2,2);
+sz = r36(3,2);
+ax = r36(1,3);
+ay = r36(2,3);
+az = r36(3,3);
+ 
+q4 = atan2(-ay, -ax)
+
+q5 = atan2(-sqrt(ax^2+ay^2), az)
+q6 = atan2(-sz, nz)
+
+
     end
 end
 % arm kinematics
@@ -67,30 +92,23 @@ f_ar_wrist = f_arm * f_wrist;
 
 
 
-
-
-
-
-
 % using inbuilt methods of robotic toolbox to find the forward kinematics
-
+qtest = [0 -pi/2 0 0 0 0];
 % reassigning theta(1) and theta(2) for plot test
-L(1) = Link([0, d(1), a(1), alpha(1), 0]);
-L(2) = Link([-pi/2, d(2), a(2), alpha(2), 0]);
-L(3) = Link([theta(3), d(3), a(3), alpha(3), 0]);
-L(4) = Link([theta(4), d(4), a(4), alpha(4), 0]);
-L(5) = Link([theta(5), d(5), a(5), alpha(5), 0]);
-L(6) = Link([theta(6), d(6), a(6), alpha(6), 0]);
+L(1) = Link([qtest(1), d(1), a(1), alpha(1), 0]);
+L(2) = Link([qtest(2), d(2), a(2), alpha(2), 0]);
+L(3) = Link([qtest(3), d(3), a(3), alpha(3), 0]);
+L(4) = Link([qtest(4), d(4), a(4), alpha(4), 0]);
+L(5) = Link([qtest(5), d(5), a(5), alpha(5), 0]);
+L(6) = Link([qtest(6), d(6), a(6), alpha(6), 0]);
 
 
-
-Fanuc1 = SerialLink(L, 'name', 'fanuc');
-fanuc_fkine= Fanuc1.fkine([0 -pi/2 theta(3) theta(4) theta(5) theta(6)]);
+Fanuc1 = SerialLink(L, 'name', 'fanucc');
+fanuc_fkine= Fanuc1.fkine(qtest);
 % plot of the pictorial representation of the robot
-Fanuc1.plot([0 -pi/2 theta(3) theta(4) theta(5) theta(6)]);
+Fanuc1.plot(qtest);
 
 
 % % % % Inverse Kinematics Solution for q1(0) and q2(0) above in linspace(-pi,pi,5)
-
 
 
